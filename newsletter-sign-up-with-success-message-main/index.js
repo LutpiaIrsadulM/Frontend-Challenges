a317095d744261ab086ae4f5e51e95cc0f2aa560
LutpiaIrsadulM <lutpiairsadulm@gmail.com>
2024-01-26 20:22:37 +0700

    newsletter challenge: Mobile view

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -2,7 +2,7 @@ function validateEmail(email) {
     const res = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     return res.test(String(email).toLowerCase());
   }
-  
+
 function validateForm() {
     event.preventDefault();
 
@@ -22,3 +22,44 @@ function validateForm() {
     }
     span.innerHTML = text;
 }
+
+function dismisSuccess() {
+    let cardForm = document.getElementById('cardForm');
+    let cardSuccess = document.getElementById('cardSuccess');
+    cardForm.style.display = 'flex';
+    cardSuccess.style.display = 'none'
+}
+
+function replaceImagePaths(path, resolution) {
+    // Switch images
+    const image = document.querySelector(".illmage");
+    let imagePath = image.getAttribute('src')
+    image.setAttribute('src', imagePath.replace(path, resolution));
+
+    // $('.illmage').each(function() {
+    //     var imagePath = $(this).attr('src');
+    //     console.log(imagePath);
+    //     $(this).attr('src', imagePath.replace("mobile", resolution));
+    //     $(this).attr('src', imagePath.replace("desktop", resolution));
+    // });
+}
+
+window.addEventListener('resize', function() {
+    var width = window.innerWidth;
+    // var height = window.innerHeight;
+
+    if (width <= 375) {
+        replaceImagePaths("/assets/images/illustration-sign-up-mobile.svg");
+    }else{
+        replaceImagePaths("/assets/images/illustration-sign-up-desktop.svg");
+    }
+    // console.log("Width: " + width + ", Height: " + height);
+});
+
+// $(window).resize(function() {
+//     checkResolution();
+// });
+
+// $(document).ready(function() {
+//     checkResolution();
+// });
